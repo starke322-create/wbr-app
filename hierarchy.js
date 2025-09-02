031b0772c93292a43f17621ca497dc4c59b30216
starke322-create <starke322@gmail.com>
2025-09-01 21:01:48 -0500

    Create hierarchy.js

---
+++ hierarchy.js
@@ -0,0 +1,557 @@
+const hierarchy = {
+  "EAST": {
+    "NORTHEAST": {
+      "HUDSON": [
+        "2242: Washington",
+        "706E: Route 9",
+        "5692: North Broadway",
+        "703E: Morris",
+        "5678: Moger",
+        "1742: Lakeview",
+        "5284: Hamburg",
+        "2776: Frederick",
+        "704E: County Road",
+        "4GSI: Bergenline"
+      ],
+      "LAKESHORE": [
+        "5GSQ: Youngstown",
+        "5GSJ: Twinsburg",
+        "2VSM: Streetsboro",
+        "5GSP: Stow",
+        "5GSO: Solon",
+        "5GSM: Independence",
+        "3USC: Hermitage",
+        "5GST: Green",
+        "2VSL: Garfield Heights",
+        "2VSH: Clifton",
+        "2VSD: Ashtabula"
+      ],
+      "LONG ISLAND": [
+        "5524: West Islip",
+        "5391: Rockaway",
+        "791E: New York Avenue",
+        "5457: Jericho",
+        "4MSU: Islandia",
+        "500E: Hempstead",
+        "700G: East Islip",
+        "1157: Commack",
+        "2077: Bethpage",
+        "4MSD: Baisley Blvd"
+      ],
+      "MASSACHUSETTS": [
+        "5485: Spencer",
+        "5634: River Road",
+        "4FSI: Prospect Hill",
+        "450E: North Street",
+        "345D: Methuen",
+        "573E: Malden",
+        "686D: Haverhill",
+        "452E: Harwich",
+        "4FSF: Glastonbury",
+        "218D: Billerica",
+        "851C: Albany Turnpike"
+      ],
+      "MID ATLANTIC": [
+        "5473: West Ruark",
+        "708D: Village Center",
+        "939D: Salisbury Blvd",
+        "5628: Montanus",
+        "5413: Marlboro",
+        "5474: Lexington",
+        "658D: Damascus",
+        "2146: Buckeystown",
+        "643E: Bristow"
+      ],
+      "PHILADELPHIA": [
+        "512E: Yard Ville",
+        "693E: Summit Square",
+        "574D: Newark DE",
+        "5662: Middletown",
+        "766D: Dilworthtown",
+        "598E: Crossing",
+        "434E: Commons",
+        "5503: Big Elk"
+      ],
+      "QUEENS": [
+        "1151: Springfield Blvd",
+        "4LSV: Ridgewood",
+        "1564: Ozone Park",
+        "4160: Main Street",
+        "4210: Linden",
+        "2771: Horace",
+        "1218: Glen Oaks",
+        "1160: Elmont",
+        "187F: College Point",
+        "1449: Broadway",
+        "532E: 8th Avenue"
+      ],
+      "STEEL VALLEY": [
+        "3TSZ: Uniontown",
+        "5HSE: Triadelphia",
+        "887E: Steubenville",
+        "878E: St Clairsville",
+        "3USA: Countryside",
+        "2VSB: Cambridge",
+        "5HSA: Beaver Falls",
+        "2VSP: Alliance"
+      ]
+    },
+    "OHIO VALLEY": {
+      "CENTRAL OHIO EAST": [
+        "926D: Westfield",
+        "227G: Washington Court House",
+        "553E: Sycamore Plaza",
+        "2USH: Sunbury",
+        "2USG: Reynoldsburg",
+        "731E: Pataskala",
+        "2USV: Mt. Gilead",
+        "2USB: Marysville",
+        "2USO: London",
+        "2VSZ: Hillsboro",
+        "2TSZ: Delaware",
+        "2USL: Circleville",
+        "964E: Canal Winchester"
+      ],
+      "CINCINNATI SOUTH": [
+        "2279: North Bend",
+        "2839: Newport",
+        "3DSI: Lawrenceburg",
+        "5DSN: Highland Heights",
+        "933D: Hebron",
+        "2WSD: Harrison Avenue",
+        "609E: Fairfield",
+        "2217: Eastgate",
+        "762E: Centennial"
+      ],
+      "INDY CITY": [
+        "2WSO: Zionsville",
+        "5533: West Lafayette",
+        "179D: Noblesville",
+        "3DSM: Muncie",
+        "3DSH: Marion",
+        "2WSP: Heartland",
+        "3CSC: Hazel Dell",
+        "2WSN: Frankfort",
+        "605E: Carmel",
+        "4629: Anderson"
+      ],
+      "INDY GP": [
+        "481E: Shelbyville",
+        "456E: Seymour",
+        "617E: Rushville",
+        "2WSS: Norgate",
+        "2WSR: Nora",
+        "732E: New Castle",
+        "472E: Martinsville",
+        "404E: Keystone",
+        "321F: Greenfield",
+        "5552: Bloomington",
+        "480E: Bargersville"
+      ],
+      "MICHIGAN EAST": [
+        "477E: South Lyon",
+        "150F: Rochester Hills",
+        "3ESA: Port Huron",
+        "3FSA: New Hudson",
+        "877E: Milford",
+        "3ESO: Mile Road",
+        "3GSD: Lake Orion",
+        "3GSL: Imlay City",
+        "3GSI: Davison",
+        "3GSH: Clio"
+      ],
+      "MICHIGAN WEST": [
+        "2XSJ: West 48th",
+        "588E: Walker",
+        "2XSP: Northland",
+        "2XSO: Muskegon",
+        "2XSN: Ludington",
+        "722E: Knapps Corner",
+        "2XSB: Greenville",
+        "473E: Grand Rapids",
+        "2XSL: Grand Haven",
+        "3GSQ: Euclid",
+        "2XSI: Big Rapids",
+        "474E: Allendale"
+      ],
+      "MI/IN": [
+        "3CSY: Warsaw",
+        "2XSH: Three Rivers",
+        "3DSB: Kendallville",
+        "3CSU: Elkhart",
+        "5LSU: Dupont",
+        "3DSE: Chapel Ridge"
+      ],
+      "OHIO SOUTHWEST": [
+        "2WSF: Xenia",
+        "2WSA: Wilmington",
+        "2USI: Urbana",
+        "2WSL: Springfield",
+        "2VSY: Springboro",
+        "2563: South Main Street",
+        "2WSE: Sidney",
+        "764E: Piqua",
+        "560E: Oxford",
+        "563E: Lebanon",
+        "918C: Hamilton",
+        "3BSV: Centerville",
+        "2422: Brandt Pike",
+        "2USW: Bellefontaine"
+      ],
+      "TRI-VALLEY": [
+        "2VSA: Zanesville",
+        "3ASX: Oregon",
+        "5OSL: Ontario",
+        "377D: Norwalk",
+        "2USD: Newark",
+        "2USC: Mt. Vernon",
+        "2USE: Heath",
+        "5GSG: Fremont",
+        "2VSC: Coshocton",
+        "2VST: Bucyrus",
+        "3ASV: Bowling Green",
+        "2VSS: Ashland"
+      ]
+    },
+    "SOUTHEAST": {
+      "ATLANTA": [
+        "5FSR: Wrightsboro",
+        "2HSF: Winder",
+        "2GSM: Peachtree City",
+        "2GSG: Locust Grove",
+        "2HSD: Epps Bridge",
+        "2ESP: Dacula",
+        "2HSA: Commerce"
+      ],
+      "ATLANTIC COAST": [
+        "2HSP: Savannah Crossing",
+        "2HSU: Macclenny",
+        "2ISN: Lowes Village",
+        "2HSQ: Brunswick",
+        "3ASM: Beaufort"
+      ],
+      "CAROLINA EAST": [
+        "2QSL: Zebulon",
+        "2QSI: Wake Forest",
+        "2QSH: Smithfield",
+        "2RSO: Morehead City",
+        "2QSE: Judd Parkway",
+        "2QSC: Fordham",
+        "2QSF: Clayton"
+      ],
+      "CAROLINA WEST": [
+        "2SNH: Greenwood",
+        "3USU: Gaffney",
+        "2RSV: Columbia",
+        "3ASC: Coastal Grand Mall",
+        "3UST: Boiling Springs",
+        "3USZ: Augusta",
+        "3ASB: 31st Ave"
+      ],
+      "FLORIDA": [
+        "5JSI: Seminole",
+        "2OSQ: Rolling Oaks",
+        "2OSH: Ocala",
+        "2TSY: Granada",
+        "2ISW: Apopka",
+        "5LSM: Andover"
+      ],
+      "NORTH GEORGIA": [
+        "2GSA: Villa Rica",
+        "2FSL: Smyrna",
+        "2FSZ: Rome",
+        "2FSD: Marietta",
+        "2HSH: Dalton",
+        "2FSW: Chapel Hill",
+        "2FSU: Cartersville",
+        "5BSV: Athens"
+      ],
+      "TN/VA": [
+        "6ESN: Oak ridge",
+        "5CSB: Morristown",
+        "5HSF: Knoxville",
+        "5CSH: Kingston Pike",
+        "5ASV: Exit Seven",
+        "5BSY: Elizabethton",
+        "5GSB: Asheville"
+      ],
+      "WINSTON SALEM": [
+        "2QSA: Thomasville",
+        "5FSV: Samet",
+        "2PSY: Kernersville",
+        "2PST: East Hanes"
+      ]
+    }
+  },
+  "WEST": {
+    "GULF COAST": {
+      "ATX CENTRAL": [
+        "1FSU: West Woods",
+        "1DST: Texas Ave",
+        "6ESP: New Braunfels",
+        "961F: Highway 71",
+        "1ESB: Creekside",
+        "545D: College Station",
+        "1FSC: Buda",
+        "1DSU: Bryan",
+        "1FSS: Belterra"
+      ],
+      "ATX NORTH": [
+        "1DSG: Taylor",
+        "1FSM: Round Rock",
+        "1FSI: Marble Falls",
+        "1FSH: Manor",
+        "1DSE: Killeen Mall",
+        "1DSF: Killeen",
+        "1DSD: Copperas Cove"
+      ],
+      "HOUSTON NORTH": [
+        "1USB: Willis",
+        "436E: Tuckerton",
+        "1TST: Tomball",
+        "1USH: Sawdust",
+        "1USD: Magnolia",
+        "6ESG: Louetta",
+        "427E: Kingwood",
+        "1USC: Huntsville",
+        "1DSR: Mont Belvieu",
+        "232E: Eva Plaza",
+        "354E: Cypress",
+        "689G: Crosby",
+        "1USM: 529"
+      ],
+      "HOUSTON SOUTH": [
+        "5HSJ: Waterview",
+        "5332: Victoria",
+        "6ESR: Texas City",
+        "1TSZ: River Oaks",
+        "2266: Parkwood",
+        "2066: Mason",
+        "1USX: Lake Jackson",
+        "1405: Highway 6",
+        "2232: Bellaire",
+        "536E: Beeville",
+        "259E: Bay City",
+        "5561: Angleton",
+        "598D: Alice"
+      ],
+      "LOUISIANA EAST": [
+        "4BST: Veterans",
+        "4BSW: Uptown",
+        "4BSX: Thibodaux",
+        "5CSX: Ridgeland",
+        "5LSV: Rangeline",
+        "4BSZ: Hammond",
+        "2064: Gentilly",
+        "4CSA: Covington",
+        "2833: Claiborne",
+        "416D: Airline"
+      ],
+      "LOUISIANA WEST": [
+        "2YSZ: Prairieville",
+        "227E: Pinhook",
+        "4810: Oneal",
+        "4CSJ: North Mall",
+        "4CSE: New Iberia",
+        "957D: Lafayette",
+        "2YSY: Denham Springs",
+        "6ESQ: Alexandria"
+      ],
+      "SATX NORTH": [
+        "776D: Thousand Oaks",
+        "767D: Spring Branch",
+        "569D: South Main",
+        "1ESD: Seguin",
+        "571D: Schertz",
+        "1ESU: La Cantera",
+        "1DSW: Kerrville",
+        "583D: Cibolo"
+      ],
+      "SATX SOUTH": [
+        "1VSE: Valley Hi",
+        "584D: Uvalde",
+        "1ESF: SW Military",
+        "1ESL: South Park Mall",
+        "5441: South Bibb",
+        "2074: Rigsby Avenue",
+        "4723: Palo Alto",
+        "1627: Marbach",
+        "5DSX: Kingsville",
+        "1DSZ: HEB-Lytle",
+        "221D: Eagle Pass"
+      ],
+      "SOUTH TEXAS": [
+        "1953: Viejo",
+        "4341: Stegner",
+        "200E: Rockport",
+        "216E: Portland",
+        "374E: Ocean Blvd",
+        "2584: North Conway",
+        "5751: Elsa"
+      ]
+    },
+    "MIDWEST": {
+      "ARKANSAS": [
+        "2ZSE: Van Buren",
+        "2ZSC: Searcy",
+        "4CSY: Russellville",
+        "4CSM: Pine Bluff",
+        "4CST: Little Rock",
+        "4CSN: Hot Springs"
+      ],
+      "KANSAS": [
+        "857C: Schilling",
+        "966D: Sapulpa",
+        "5742: Riverside Pkwy",
+        "407D: Ponca City",
+        "687E: Peoria Ave",
+        "849C: George Washington",
+        "113E: Broken Arrow"
+      ],
+      "KENTUCKY EAST": [
+        "240E: Winchester",
+        "6ESE: New Circle",
+        "144E: Maysville",
+        "5DSL: Leestown",
+        "918E: La Grange",
+        "2564: Jeffersonville",
+        "5DSF: Georgetown",
+        "5DSI: Danville",
+        "3DSJ: Corydon"
+      ],
+      "KENTUCKY WEST": [
+        "5518: Springhurst",
+        "5DSR: Owensboro",
+        "894E: Mt Washington",
+        "5CSZ: Louisville",
+        "3DSO: Evansville Burk",
+        "5DSE: Dixie"
+      ],
+      "MISSISSIPPI RIVER VALLEY": [
+        "3ISY: Virginia Avenue",
+        "4ASP: Rolla",
+        "5DSO: Paducah",
+        "4ASM: Osage",
+        "4ASN: Jeff City",
+        "4ASA: Hannibal",
+        "3ISS: Dubuque",
+        "5DSP: 12th Street"
+      ],
+      "NEBRASKA": [
+        "4BSH: Maple",
+        "4BSP: King Lane",
+        "4BSR: Grand Island",
+        "3ISR: Council Bluffs",
+        "4BSM: Center",
+        "4BSN: Ames",
+        "3ISH: Altoona",
+        "4BSI: 180th Street"
+      ],
+      "OKC": [
+        "990F: Wrangler",
+        "4789: Owen Garriott",
+        "4DSC: Mustang",
+        "4781: Market Place",
+        "5280: Garth Brooks Blvd",
+        "114E: Chickasha",
+        "4320: Bartlesville",
+        "586D: Ardmore"
+      ]
+    },
+    "SOUTHWEST": {
+      "ARIZONA SOUTH": [
+        "1WSO: Tucson Fashion Park",
+        "6ESM: Nogales South",
+        "6ESO: Las Plazas",
+        "6ESL: Harrison Plaza",
+        "1WSQ: Grant Road",
+        "1WSN: Cochise"
+      ],
+      "AZ-EAST": [
+        "4031: Talking",
+        "2843: Stapley",
+        "1JSN: Red Mountain",
+        "5OSO: Power",
+        "701F: Coolidge",
+        "5685: Combs",
+        "2030: Apache"
+      ],
+      "AZ-WEST": [
+        "4832: Union",
+        "1994: Northern Avenue",
+        "2173: Mcdowell",
+        "2961: Laveen",
+        "0526: Desert",
+        "2029: Central",
+        "5431: 99th Avenue",
+        "1WSJ: 303 & Waddell"
+      ],
+      "COLORADO": [
+        "5DSZ: Peoria",
+        "5ESS: Northgate",
+        "5ESI: Highpointe",
+        "166E: Frontier",
+        "5ESN: Fountain",
+        "5ESO: Falcon",
+        "5ESL: Dillon",
+        "5ESX: Cheyenne"
+      ],
+      "DFW NORTH": [
+        "5784: Wesley",
+        "2819: Walnut",
+        "465D: Stonebrook",
+        "1357: Richardson TX",
+        "618E: Pines Road",
+        "904E: Jefferson",
+        "472D: Independence Pkwy",
+        "1TSF: Dallas Pkwy",
+        "875E: Custer"
+      ],
+      "DFW SOUTH": [
+        "5429: Seventh Ave",
+        "1BSE: Red Oak",
+        "1BSA: Ennis",
+        "0118: Eastchase",
+        "1239: Duncanville",
+        "5761: Corsicana Hwy",
+        "2536: Cooper Street",
+        "1CSD: Bedford"
+      ],
+      "DFW WEST": [
+        "1CSM: Weatherford",
+        "256E: Stephenville",
+        "0115: Rufe Snow",
+        "924C: Granbury",
+        "5564: Golden Triangle",
+        "2993: Clifford",
+        "211E: Cleburne",
+        "6ESH: Chisholm Trail",
+        "985D: 28th Street"
+      ],
+      "DESERT MOUNTAIN": [
+        "720F: Sedona",
+        "5333: Prescott Valley East",
+        "4506: Highway 89",
+        "1LSI: Flagstaff",
+        "1LSS: Farmington"
+      ],
+      "PAN HANDLE": [
+        "922E: Walmart Court",
+        "1FSZ: S. Georgia",
+        "1FSX: Plainview",
+        "903E: Olton",
+        "269E: Odessa",
+        "705D: Midland",
+        "930E: Big Spring"
+      ],
+      "UTAH/CO": [
+        "1ISF: Vernal",
+        "1ISA: Lehi",
+        "1VSW: Layton",
+        "5ESV: Grand Junction",
+        "5ESW: Glenwood Springs",
+        "1HSY: Draper"
+      ]
+    }
+  }
+};
